abec5135a38439c65cc06dbf76ca78d628b431cf
qibao <chenweiwen07@foxmail.com>
2024-06-11 17:19:27 +0800

    update

--- start.sh
+++ start.sh
@@ -2,6 +2,13 @@
 
 skopeo --version
 
-while IFS= read -r line; do
-  echo $line
-done < images.txt
+while IFS= read -r line
+do
+  [[ -z "$line" ]] && continue
+  echo "$line"
+  image=$(echo "$line" | awk '{print $NF}')
+  image_name_tag=$(echo "$image" | awk -F'/' '{print $NF}')
+  skopeo copy --all --dest-creds ${ALIYUN_REGISTRY_USER}:${ALIYUN_REGISTRY_PASSWORD} \
+    docker://${image} \
+    docker://${ALIYUN_REGISTRY}/${ALIYUN_NAME_SPACE}/${image_name_tag}
+done < "images.txt"

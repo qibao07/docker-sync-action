5f0eec03e232d17a00cdb670e1bbdf96c848c016
qibao <chenweiwen07@foxmail.com>
2024-06-13 10:00:11 +0800

    update

--- start.sh
+++ start.sh
@@ -2,13 +2,22 @@
 
 skopeo --version
 
+mkdir -p finish.images
+
 while IFS= read -r line
 do
   [[ -z "$line" ]] && continue
-  echo "$line"
-  image=$(echo "$line" | awk '{print $NF}')
+  force=$(echo "$line" | awk '{print $2}')
+  image=$(echo "$line" | awk '{print $1}')
   image_name_tag=$(echo "$image" | awk -F'/' '{print $NF}')
-  skopeo copy --all --dest-creds ${ALIYUN_REGISTRY_USER}:${ALIYUN_REGISTRY_PASSWORD} \
-    docker://${image} \
-    docker://${ALIYUN_REGISTRY}/${ALIYUN_NAME_SPACE}/${image_name_tag}
+  image_file_name="finish.images/$(echo "$image" | sed 's/[\/:]/./g')"
+  if [[ -n $force ]] || [ ! -e "$image_file_name" ]; then
+    echo "同步镜像：$image"
+    skopeo copy --all --dest-creds ${ALIYUN_REGISTRY_USER}:${ALIYUN_REGISTRY_PASSWORD} \
+      docker://${image} \
+      docker://${ALIYUN_REGISTRY}/${ALIYUN_NAME_SPACE}/${image_name_tag}
+    touch ${image_file_name}
+  else
+    echo "跳过镜像：$image"
+  fi
 done < "images.txt"

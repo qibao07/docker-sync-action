41ca7c6c9cdadb55fc7dd7c9927d9da949c1aa35
qibao <chenweiwen07@foxmail.com>
2024-09-20 15:02:38 +0800

    update

--- start.sh
+++ start.sh
@@ -14,12 +14,18 @@ do
   image=$(echo "$line" | awk '{print $1}')
   image_name_tag=$(echo "$image" | awk -F'/' '{print $NF}')
   image_file_name="finish.images/$(echo "$image" | sed 's/[\/:]/./g')"
-  if [[ -n $force ]] || [ "$image" == "$force_image" ] || [ ! -e "$image_file_name" ]; then
+  image_digest=$(skopeo inspect docker://${image} | grep Digest | head -n 1 | awk -F '"' '{print $4}')
+  # 镜像复制触发条件
+  # 1. images.txt指定force
+  # 2. 手动触发参数指定
+  # 3. 缓存未标记完成
+  # 4. 缓存标记但digest发生变化
+  if [[ -n $force ]] || [ "$image" == "$force_image" ] || [ ! -e "$image_file_name" ] || [ "$(cat $image_file_name)" != "$image_digest" ]; then
     echo "同步镜像：$image"
     skopeo copy --all --dest-creds ${ALIYUN_REGISTRY_USER}:${ALIYUN_REGISTRY_PASSWORD} \
       docker://${image} \
       docker://${ALIYUN_REGISTRY}/${ALIYUN_NAME_SPACE}/${image_name_tag}
-    touch ${image_file_name}
+    echo "$image_digest" > ${image_file_name}
   else
     echo "跳过镜像：$image"
   fi

b87b5176594140b9bc6b05b16a586f653b3ee9cf
qibao <chenweiwen07@foxmail.com>
2025-03-06 13:50:42 +0800

    update

--- start.sh
+++ start.sh
@@ -6,12 +6,9 @@ skopeo --version
 
 mkdir -p finish.images
 
-while IFS= read -r line
-do
-  [[ -z "$line" ]] && continue
-  [[ $line =~ ^# ]] && continue
-  force=$(echo "$line" | awk '{print $2}')
-  image=$(echo "$line" | awk '{print $1}')
+function syncImage() {
+  force=$2
+  image=$1
   image_name_tag=$(echo "$image" | awk -F'/' '{print $NF}')
   image_file_name="finish.images/$(echo "$image" | sed 's/[\/:]/./g')"
   image_digest=$(skopeo inspect docker://${image} | grep Digest | head -n 1 | awk -F '"' '{print $4}')
@@ -20,7 +17,7 @@ do
   # 2. 手动触发参数指定
   # 3. 缓存未标记完成
   # 4. 缓存标记但digest发生变化
-  if [[ -n $force ]] || [ "$image" == "$force_image" ] || [ ! -e "$image_file_name" ] || [ "$(cat $image_file_name)" != "$image_digest" ]; then
+  if [[ -n "$force" ]] || [ ! -e "$image_file_name" ] || [ "$(cat $image_file_name)" != "$image_digest" ]; then
     echo "同步镜像：$image"
     skopeo copy --all --dest-creds ${ALIYUN_REGISTRY_USER}:${ALIYUN_REGISTRY_PASSWORD} \
       docker://${image} \
@@ -29,4 +26,17 @@ do
   else
     echo "跳过镜像：$image"
   fi
+}
+
+fi [[ -n "$force_image" ]]; then
+  syncImage "$image" "1"
+fi
+
+while IFS= read -r line
+do
+  [[ -z "$line" ]] && continue
+  [[ $line =~ ^# ]] && continue
+  force=$(echo "$line" | awk '{print $2}')
+  image=$(echo "$line" | awk '{print $1}')
+  syncImage "$image" "$force"
 done < "images.txt"
